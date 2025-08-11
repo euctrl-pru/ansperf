46bd39e6f1464d867f0d37d8bae85ee30acd41df
Rainer Koellee <rainer.koelle@eurocontrol.int>
2025-08-11 11:11:02 +0200

    clean check notes

--- R/ansperf-package.R
+++ R/ansperf-package.R
@@ -3,5 +3,6 @@
 
 ## usethis namespace: start
 #' @importFrom rlang .data
+#' @importFrom readr read_csv
 ## usethis namespace: end
 NULL
--- data-src/_README.txt
+++
@@ -1,2 +0,0 @@
-README to create the data-src folder.
-  This folder hosts some larger data sets for the development of the package.

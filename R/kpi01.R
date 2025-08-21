df26fa365ea501c5312e2d2813eb86edc37e1c1f
Rainer Koellee <rainer.koelle@eurocontrol.int>
2025-08-21 15:38:02 +0200

    post coord call

--- R/kpi01.R
+++ R/kpi01.R
@@ -1,18 +1,15 @@
-################################################################################
-# Script:         kpi01.R
-# Author:         Jean Lima
-# Date:           2025-07-30
-# Description:    Implementation of KPI01
-################################################################################
 
-#' @export
-
-library(tidyverse)
-library(dplyr)
 
 
 # Calculates the delay for each flight
-calculate_delay <- function(df) {
+#' Title
+#'
+#' @param df
+#'
+#' @return
+#' @export
+#'
+prepare_delay <- function(df) {
   df$delay <- as.numeric(
     difftime(
       as.POSIXct(df$atot, format = "%Y-%m-%d %H:%M:%OS"),
@@ -26,26 +23,29 @@ calculate_delay <- function(df) {
 
 # Checks which variants classify each flight as on time
 check_variants <- function(df) {
-  df$v1a <- ifelse(df$delay >= -5 & df$delay <= 5, 1, 0)
-  df$v1b <- ifelse(df$delay < 5, 1, 0)
-  df$v2a <- ifelse(df$delay >= -15 & df$delay <= 15, 1, 0)
-  df$v2b <- ifelse(df$delay < 15, 1, 0)
+  df <- df |>
+    dplyr::mutate(
+       .data$v1a <- ifelse(.data$delay >= -5 & .data$delay <= 5, 1, 0)
+      ,.data$v1b <- ifelse(delay < 5, 1, 0)
+    )
+  #df$v2a <- ifelse(df$delay >= -15 & df$delay <= 15, 1, 0)
+  #df$v2b <- ifelse(df$delay < 15, 1, 0)
 
   return(df)
 }
 
 
-# Calculates KPI01 -- Percentage of on-time flights for each variant
-calculate_kpi01 <- function(df) {
-  kpi01_df <- df %>%
-    group_by(adep) %>%
-    summarise(
-      kpi01_v1a = sum(v1a == 1, na.rm = TRUE) / n(),
-      kpi01_v1b = sum(v1b == 1, na.rm = TRUE) / n(),
-      kpi01_v2a = sum(v2a == 1, na.rm = TRUE) / n(),
-      kpi01_v2b = sum(v2b == 1, na.rm = TRUE) / n()
-    ) %>%
-    ungroup()
-
-  return(kpi01_df)
-}
+# # Calculates KPI01 -- Percentage of on-time flights for each variant
+# calculate_punctualtiy <- function(df) {
+#   kpi01_df <- df |>
+#     dplyr::group_by(adep) |>
+#     dplyr::summarise(
+#       kpi01_v1a = sum(v1a == 1, na.rm = TRUE) / n(),
+#       kpi01_v1b = sum(v1b == 1, na.rm = TRUE) / n(),
+#       kpi01_v2a = sum(v2a == 1, na.rm = TRUE) / n(),
+#       kpi01_v2b = sum(v2b == 1, na.rm = TRUE) / n()
+#     ) |>
+#     dplyr::ungroup()
+#
+#   return(kpi01_df)
+# }
---
+++ R/taxi-times.R
@@ -0,0 +1,9 @@
+# we want to code taxi algo here
+
+# RESHAPE
+
+# PREPARE data to do and process for establishing the analytic data
+
+# establish ANALYTIC DATA
+
+# calculate the indicator stats
